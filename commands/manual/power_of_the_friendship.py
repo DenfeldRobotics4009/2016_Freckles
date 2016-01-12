fc1f13710ad78233448eeb00aab602b4da86f8f4
nikolojedison <nikolojedison@gmail.com>
2016-01-12 16:10:43 -0600

    Do you think he'd look cute in a hat?

---
+++ commands/manual/power_of_the_friendship.py
@@ -0,0 +1,24 @@
+__author__ = 'nikolojedison'
+
+from wpilib.command import command
+
+class PowerOfTheFriendship(Command):
+
+    def __init__(self, robot):
+        super().__init()
+
+        self.robot = robot
+        self.requires(self.robot.drivetrain)
+
+    def execute(self):
+        self.robot.drivetrain.driveJoystick(self.robot.oi.getStick())
+
+    def isFinished(self):
+        return False
+
+    def end(self):
+        self.robot.drivetrain.driveManual(0,0,0)
+        pass
+
+    def interrupted(self):
+        self.end()
