a02fec21b536470539f1a815b2c1aec185dda33f
nikolojedison <nikolojedison@gmail.com>
2016-01-18 12:57:57 -0600

    woohoo sensitivity.

--- robot.py
+++ robot.py
@@ -6,6 +6,8 @@ import networktables
 from wpilib.command import Scheduler
 from oi import OI
 
+from utilities.settings import Settings
+
 from subsystems.drivetrain import Drivetrain
 
 class Freckles(wpilib.SampleRobot):
@@ -14,7 +16,7 @@ class Freckles(wpilib.SampleRobot):
     def robotInit(self):
         self.drivetrain = Drivetrain(self)
         self.oi = OI(self)
-        self.sensitivity = self.oi.smart_dashboard.getInt("Sensitivity", 5)
+        self.sensitivity = self.oi.smart_dashboard.getInt("Sensitivity", 6)
 
     def autonomous(self):
         while self.isAutonomous() and self.isEnabled():
@@ -26,23 +28,32 @@ class Freckles(wpilib.SampleRobot):
         joystick = self.oi.getStick()
         try:
             if self.sensitivity == 1:
-                pass
+                Settings.num_scaling = 0.10
+                Settings.num_drive = 5
             elif self.sensitivity == 2:
-                pass
+                Settings.num_scaling = 0.25
+                Settings.num_drive = 4
             elif self.sensitivity == 3:
-                pass
+                Settings.num_scaling = 0.45
+                Settings.num_drive = 4
             elif self.sensitivity == 4:
-                pass
+                Settings.num_scaling = 0.60
+                Settings.num_drive = 4
             elif self.sensitivity == 5:
-                pass
+                Settings.num_scaling = 0.75
+                Settings.num_drive = 4
             elif self.sensitivity == 6:
-                pass
+                Settings.num_scaling = 1.25
+                Settings.num_drive = 3
             elif self.sensitivity == 7:
-                pass
+                Settings.num_scaling = 1.5
+                Settings.num_drive = 3
             elif self.sensitivity == 8:
-                pass
+                Settings.num_scaling = 1.85
+                Settings.num_drive = 3
             elif self.sensitivity == 9:
-                pass
+                Settings.num_scaling = 2.3
+                Settings.num_drive = 2
             else:
                 pass
         except KeyError:
