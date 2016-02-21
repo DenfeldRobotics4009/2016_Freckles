c01ed8daf6728ff791efac7dddb1f45df78e9aec
nikolojedison <nikolojedison@gmail.com>
2016-02-21 11:25:51 -0600

    Precision update.

--- utilities/settings.py
+++ utilities/settings.py
@@ -24,7 +24,7 @@ class Settings():
     """Robot mapping. Values that are changed often go here."""
 
     #Numbers to be changed through drive station
-    num_precision_one = 0.80
+    num_precision_one = 0.60
     num_precision_two = 0.50
     num_scaling = 1.25
     num_macro_timeout = 15
