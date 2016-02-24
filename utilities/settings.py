8ee4d5f2fb82dd85be29304873e1602822991db1
nikolojedison <nikolojedison@gmail.com>
2016-02-23 20:16:39 -0600

    uggggh

--- utilities/settings.py
+++ utilities/settings.py
@@ -8,6 +8,7 @@ class Settings():
     num_precision_two = 0.50
     num_scaling = 1.25
     num_macro_timeout = 15
+    str_macro_name = "macro.csv"
 
     #Tilt pot setpoints .158
     kMaxDown = 0.69 #derp, was .790
